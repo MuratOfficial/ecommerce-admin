f904155a80a83fe241405460a2d9365f9bdc4d00
MuratOfficial <toimetm@gmail.com>
2023-09-12 18:47:01 +0600

    added size and color entities

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,12 @@
 import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+// import type { Metadata } from "next";
+
+// export const metadata: Metadata = {
+//   title: "Admin Dashboard",
+//   description: "Admin Dashboard Panel for managing entities of Store",
+// };
 
 export default async function HomeLayout({
   children,
