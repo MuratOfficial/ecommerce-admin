83cce155f076e2258206cbbebbb5547de1d8b55c
MuratOfficial <toimetm@gmail.com>
2023-12-01 19:17:40 +0600

    added components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
+import ClientNav from "@/components/client/client-nav";
+import PhotoCollage from "@/components/client/photo-collage";
+import PopularCategory from "@/components/client/popular-category";
+import ProcessChain from "@/components/client/process-chain";
+import ProductLinks from "@/components/client/product-links";
 import React from "react";
 
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <ClientNav />
+      <PhotoCollage />
+      <PopularCategory />
+      <ProductLinks />
+      {/**Animated Blocks */}
+      <ProcessChain />
+    </div>
+  );
 }
 
 export default HomePage;
